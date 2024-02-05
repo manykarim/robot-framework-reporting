0ef8ecf334e73751bc28013bf7a308eccea68055
Many Kasiriha <many.kasiriha@gmail.com>
2024-02-05 21:57:22 +0000

    Add same network

--- start_influxdb.sh
+++ start_influxdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 8086:8086 -v "$PWD/influx-data:/var/lib/influxdb2" influxdb:latest
+docker run -d -p 8086:8086 -v "$PWD/influx-data:/var/lib/influxdb2" --network=robot influxdb:latest
--- start_postgresql.sh
+++ start_postgresql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=robotframework -e PGDATA=/var/lib/postgresql/data/pgdata -v "$PWD/postgresql-data:/var/lib/postgresql/data"	postgres
+docker run -d -p 5432:5432 --name=postgres -e POSTGRES_PASSWORD=robotframework -e PGDATA=/var/lib/postgresql/data/pgdata -v "$PWD/postgresql-data:/var/lib/postgresql/data"	--network=robot postgres
