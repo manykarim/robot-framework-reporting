3c0aba9b611c779ec0babcebacc45fb7c2e54103
Many Kasiriha <many.kasiriha@gmail.com>
2024-02-13 21:37:10 +0000

    Add name

--- start_influxdb.sh
+++ start_influxdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 8086:8086 -v "$PWD/influx-data:/var/lib/influxdb2" --network=robot influxdb:latest
+docker run -d -p 8086:8086 --name=influx -v "$PWD/influx-data:/var/lib/influxdb2" --network=robot influxdb:latest
