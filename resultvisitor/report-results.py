55571e0c4d18fd525f7088cc8f60aafff327b143
Many Kasiriha <many.kasiriha@gmail.com>
2024-02-13 19:10:26 +0000

    Add test suite stats

---
+++ resultvisitor/report-results.py
@@ -0,0 +1,41 @@
+from robot.api import ExecutionResult, ResultVisitor
+import sys
+
+class TestFinder(ResultVisitor):
+
+    def __init__(self, markdown_file):
+        self.f = open(f"{markdown_file}", "w")
+
+    def start_result(self, result):
+        stats = result.statistics.total
+        self.f.write("# Overview\n")
+        self.f.write("| Passed | Failed | Skipped |\n")
+        self.f.write("| --- | --- | --- |\n")
+        self.f.write(f"| {stats.passed} | {stats.failed} | {stats.skipped} |\n\n")
+
+    def start_suite(self, suite):
+        if suite.tests:
+            stats = suite.statistics
+            self.f.write(f"## {suite.name}\n")
+            self.f.write("| Passed | Failed | Skipped |\n")
+            self.f.write("| --- | --- | --- |\n")
+            self.f.write(f"| {stats.passed} | {stats.failed} | {stats.skipped} |\n\n")
+            self.f.write("| Test | Status | Elapsed |\n")
+            self.f.write("| --- | --- | --- |\n")
+    
+    def start_test(self, test):
+        self.f.write(f"| {test.name} | {test.status} | {test.elapsed_time.total_seconds()} |\n")
+
+    def end_suite(self,suite):
+        if suite.tests:
+            self.f.write("\n")      
+
+def main():
+    output_file = sys.argv[1]
+    markdown_file = sys.argv[2]
+    result = ExecutionResult(output_file)
+    visitor = TestFinder(markdown_file)
+    result.visit(visitor)
+
+if __name__ == "__main__":
+    main()
