6d26904fed44a9e54acc86087fdb5ffccf3267d6
Many Kasiriha <many.kasiriha@gmail.com>
2024-02-24 10:04:59 +0000

    Allow unsigned plugin

--- docker/start_grafana.sh
+++ docker/start_grafana.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -d -p 3000:3000 --name=grafana  --user "$(id -u)"  --volume "$PWD/grafana-data:/var/lib/grafana"  --network=robot grafana/grafana-enterprise
+docker run -d -p 3000:3000 --name=grafana \
+--user "$(id -u)" \
+--volume "$PWD/grafana-data:/var/lib/grafana" \
+-e GF_PLUGINS_ALLOW_LOADING_UNSIGNED_PLUGINS=volkovlabs-variable-panel \
+--network=robot \
+grafana/grafana-enterprise
